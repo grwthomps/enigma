04ff720ed4ea28f84541513b55125cc055c3879b
Graham Thompson <grwthomps@gmail.com>
2019-09-12 17:41:40 -0600

    Add test for creating random key

--- test/random_generator_test.rb
+++ test/random_generator_test.rb
@@ -1,5 +1,16 @@
+require './test/test_helper'
 require './lib/random_generator'
 
 class RandomGeneratorTest < Minitest::Test
+
+  def test_creating_random_key
+    # RandomGenerator.stubs(:create_key).returns("00000")
+    # # want to stub 5 digit number
+    assert_equal 5, RandomGenerator.create_key.length
+  end
+
+  def test_creating_random_date
+    skip
+  end
 
 end
