792bf7c72f362a1fc1427412ac625e9422dc3a77
Graham Thompson <grwthomps@gmail.com>
2019-09-13 11:17:11 -0600

    Attempt to mock and stub self.create_key unsuccessfully, comment out

--- test/random_generator_test.rb
+++ test/random_generator_test.rb
@@ -1,12 +1,17 @@
 require './test/test_helper'
 require './lib/random_generator'
+require 'mocha/minitest'
 
 class RandomGeneratorTest < Minitest::Test
 
   def test_creating_random_key
+    # RandomGenerator = mock
     # RandomGenerator.stubs(:create_key).returns("00000")
     # # want to stub 5 digit number
+    # assert_equal "00000", RandomGenerator.create_key
+    # stub that rand is being run?
     assert_equal 5, RandomGenerator.create_key.length
+    assert_equal String, RandomGenerator.create_key.class
   end
-  
+
 end
