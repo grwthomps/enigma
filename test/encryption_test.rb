af78a492fe9251c112a2d9b0e0321aa148a9b18e
Graham Thompson <grwthomps@gmail.com>
2019-09-14 16:37:24 -0600

    Add test for shift_letters helper

--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -8,4 +8,10 @@ class EncryptionTest < Minitest::Test
     assert_equal [3, 27, 73, 20], Encryption.shift_nums_helper(["0", "2", "7", "1", "5"], "1025")
   end
 
+  def test_shift_letters
+    alpha_arr = ("a".."z").to_a << " "
+    orig_message_arr = ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"]
+    assert_equal "keder ohulw", Encryption.shift_letters_helper(alpha_arr, orig_message_arr, [3, 27, 73, 20])
+  end
+
 end
