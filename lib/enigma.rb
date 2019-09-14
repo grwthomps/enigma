9fbdd75b073afab303a105fb9ff60881185c345e
Graham Thompson <grwthomps@gmail.com>
2019-09-14 12:27:52 -0600

    Create empty methods for encrypt and decrypt in Enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,11 @@
 class Enigma
 
+  def encrypt(enc_message, key = RandomGenerator.create_key, date = DateGenerator.create_date)
+
+  end
+
+  def decrypt(dec_message, key, date = DateGenerator.create_date)
+
+  end
+
 end
