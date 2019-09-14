ec662e81a8f2284012d71f9b6cf000c5859cd069
Graham Thompson <grwthomps@gmail.com>
2019-09-14 16:00:05 -0600

    Add test for shift_nums_helper method

--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -4,4 +4,8 @@ require 'mocha/minitest'
 
 class EncryptionTest < Minitest::Test
 
+  def test_shift_nums
+    assert_equal [3, 27, 73, 20], Encryption.shift_nums_helper(["0", "2", "7", "1", "5"], "1025")
+  end
+
 end
