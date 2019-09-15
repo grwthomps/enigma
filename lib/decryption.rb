05e8eb99b3463c9cf3d198fa3d361b9547e1f63b
Graham Thompson <grwthomps@gmail.com>
2019-09-15 14:00:37 -0600

    Create self.shift_nums_helper method and start self.decrypt method (unfinished)

---
+++ lib/decryption.rb
@@ -0,0 +1,19 @@
+class Decryption
+
+  def self.shift_nums_helper(key_arr, date_last_four)
+    shift_nums_arr = []
+    shift_nums_arr << (key_arr[0] + key_arr[1]).to_i + date_last_four[0].to_i
+    shift_nums_arr << (key_arr[1] + key_arr[2]).to_i + date_last_four[1].to_i
+    shift_nums_arr << (key_arr[2] + key_arr[3]).to_i + date_last_four[2].to_i
+    shift_nums_arr << (key_arr[3] + key_arr[4]).to_i + date_last_four[3].to_i
+    shift_nums_arr
+  end
+
+  def self.decrypt(message_input, key, date)
+    alpha_arr = ("a".."z").to_a << " "
+    message_arr = message_input.split('')
+    key_arr = key.split('')
+    date_last_four = ((date.to_i * date.to_i).to_s)[-4..-1]
+  end
+
+end
