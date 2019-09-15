36064aefc5e8f0114ccb743ebaeb55574bc397f9
Graham Thompson <grwthomps@gmail.com>
2019-09-15 14:11:01 -0600

    Create shift_letters_helper method in Decrypt class

--- lib/decryption.rb
+++ lib/decryption.rb
@@ -9,11 +9,28 @@ class Decryption
     shift_nums_arr
   end
 
+  def self.shift_letters_helper(alpha_arr, message_arr, shift_nums_arr)
+    dec_message = []
+    message_arr.each do |character|
+      if alpha_arr.include?(character)
+        shift_num = alpha_arr.index(character)
+        shift_1 = alpha_arr.rotate(shift_num)
+        shift_2 = shift_1.rotate(-(shift_nums_arr[0]))
+        dec_message << shift_2[0]
+        shift_nums_arr.rotate!
+      else
+        dec_message << character
+      end
+    end
+    dec_message.join
+  end
+
   def self.decrypt(message_input, key, date)
     alpha_arr = ("a".."z").to_a << " "
     message_arr = message_input.split('')
     key_arr = key.split('')
     date_last_four = ((date.to_i * date.to_i).to_s)[-4..-1]
+    shift_nums_arr = shift_nums_helper(key_arr, date_last_four)
   end
 
 end
