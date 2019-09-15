f76129b7136d2004e47ec0cecc0f75309d1d69b4
Graham Thompson <grwthomps@gmail.com>
2019-09-15 14:10:34 -0600

    Add test for shift_letters_helper in Decrypt class

--- test/decryption_test.rb
+++ test/decryption_test.rb
@@ -8,7 +8,14 @@ class DecryptionTest < Minitest::Test
     assert_equal [3, 27, 73, 20], Decryption.shift_nums_helper(["0", "2", "7", "1", "5"], "1025")
   end
 
-  def test_encrypt
+  def test_shift_letters
+    alpha_arr = ("a".."z").to_a << " "
+    message_arr = ["k", "e", "d", "e", "r", " ", "o", "h", "u", "l", "w"]
+    assert_equal "hello world", Decryption.shift_letters_helper(alpha_arr, message_arr, [3, 27, 73, 20])
+  end
+
+  def test_decrypt
+    skip
     assert_equal "hello world", Decryption.decrypt("keder ohulw", "02715", "040895")
   end
 
