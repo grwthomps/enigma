2f7f39dbc92c0ae2d649243efb499e85d2817756
Graham Thompson <grwthomps@gmail.com>
2019-09-16 16:21:39 -0600

    Command line interface successfully encrypting text

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,1 +1,17 @@
+require './lib/enigma'
 
+enigma = Enigma.new
+
+handle = File.open(ARGV[0], "r")
+
+incoming_text = handle.read
+
+handle.close
+
+encrypted_message = (enigma.encrypt(incoming_text))[:encryption]
+
+writer = File.open(ARGV[1], "w")
+
+writer.write(encrypted_message)
+
+writer.close
