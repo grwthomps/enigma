d9a72bd67f387ac21e04dbcd92c49423f81970c1
Graham Thompson <grwthomps@gmail.com>
2019-09-16 16:29:19 -0600

    Add return confirmation for command line interface

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -15,3 +15,5 @@ writer = File.open(ARGV[1], "w")
 writer.write(encrypted_message)
 
 writer.close
+
+puts "Created '#{ARGV[1]}' with the key #{(enigma.encrypt(incoming_text))[:key]} and date #{(enigma.encrypt(incoming_text))[:date]}"
