751e16a33e57a45b55ef43f902efef80d6501f83
Graham Thompson <grwthomps@gmail.com>
2019-09-17 18:41:05 -0600

    Add test for cracking with date

--- test/crack_test.rb
+++ test/crack_test.rb
@@ -4,4 +4,11 @@ require 'mocha/minitest'
 
 class CrackTest < Minitest::Test
 
+  def test_cracking_with_date
+    encrypted_message = "muy'bfqd mpi:fdxnfgd ropryknx bpxdcinx!pnto"
+    expected = "don't forget: the world is your oyster! end"
+
+    assert_equal expected, Crack.decrypt(encrypted_message, "170919")
+  end
+
 end
